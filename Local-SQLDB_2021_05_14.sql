971e4ca014bd634d4aea23a633e151c4853fdc37
BG_KANG <79246255+lovelybg0506@users.noreply.github.com>
2021-05-18 17:54:21 +0900

    2021-05-18

--- Local-SQLDB_2021_05_14.sql
+++ Local-SQLDB_2021_05_14.sql
@@ -219,6 +219,7 @@ firstname varchar2(20),
 lastname varchar2(25),
 phone varchar2(20)
 );
+
 insert into testtbl4
 select employee_id,first_name,last_name,phone_number
 from hr.employees;
@@ -235,9 +236,181 @@ select*from testtbl4 order by firstname;
 
 update testtbl4
 set phone='없음'
+--where firstname='david';
+where Firstname='David';
+
+update testtbl5
+set phone_number='없음';
+
+ROLLBACK;
+commit;
+
+delete from testtbl4;
+select * from tab;
+rollback;
+
+delete from testtbl4 where firstname='Peter';
+rollback;
+
+delete from testtbl4 where firstname='Peter' and rownum <=2;
+select * from testtbl4 order by firstname;
+rollback;
+
+select * from testtbl4;
+select * from testtbl4 where firstname='Peter';
+
+truncate table testtbl4; -- truncate(자르다) : 롤백 해도 데이터가 복구되지 않는다.(이유 : 자동commit 되기때문)
+
+select sysdate from dual; -- dual : 가상테이블(큰 의미는없다)
+select to_char(sysdate,'yyyy/mm/dd hh:mi:ss') "현재 날짜" from dual;
+
+select length('한글'),length('AB'),lengthb('한글'),lengthb('AB')from dual;
+
+--select
+--from
+--where
+--group by
+--having
+--order by
 
 
+--순위 함수
+select row_number()over(order by height desc) "키큰 순위",username,addr,height from usertbl;
+
+--select username,addr,height
+--from usertbl
+--order by height desc;
+
+select row_number()over(order by height desc,username asc) "키큰 순위",username,addr,height from usertbl;
+
+commit;
+
+select addr,row_number()over(partition by addr order by height desc,username asc) "지역별 키큰순위",username,addr,height from usertbl;-- partition 나누다
+
+select rank()over(order by height desc) "키큰 순위",username,addr,height from usertbl;
+
+--그룹분할,남는 인원은 다시 처음부터 채운다
+select ntile(2) over(order by height desc)"반번호",username,addr,height from usertbl; -- 두갈래
+select ntile(3) over(order by height desc)"반번호",username,addr,height from usertbl; -- 세갈래
+select ntile(4) over(order by height desc)"반번호",username,addr,height from usertbl; -- 네갈래
+
+--inner join --
+--select <열 목록>
+--from <첫 번째 테이블>
+--    inner join<두 번째 테이블>
+--    on <조인될 조건>
+--where 검색 조건
+
+-- 구매테이블 중에서 JYP라는 아이디를 가진 사람이 구매한 물건을 발송하기 위해서
+-- 이름,주소,연락처 등을 조인해서 검색
+select username,addr,mobile1,mobile2
+from buytbl
+    inner join usertbl
+    on buytbl.userid=usertbl.userid;
+--where buytbl.userid='jyp';
+
+select *
+from buytbl
+    inner join usertbl
+    on buytbl.userid=usertbl.userid;
+
+select buytbl.userid,username,prodname,addr,mobile1||mobile2 as "연락처" -- 컬럼이 겹치면(userid) 오류가 난다.
+from buytbl
+    inner join usertbl
+    on buytbl.userid=usertbl.userid;
+
+-- 테이블 명 별칭 지정하기
+select B.userid,U.username,B.prodname,U.addr,U.mobile1||U.mobile2 as "연락처"
+from buytbl B
+    inner join usertbl U
+    on B.userid=U.userid;
+
+select B.userid,U.username,B.prodname,U.addr,U.mobile1,U.mobile2 as "연락처"
+from buytbl B
+    inner join usertbl U
+    on B.userid=U.userid
+where B.userid='jyp';
+
+--회원 테이블을 기준으로 JYP라는 아이디가 구매한 물건 목록
+select U.userid,U.username,B.prodname,U.addr,U.mobile1,U.mobile2 as "연락처"
+from usertbl U
+    inner join buytbl B
+    on U.userid=B.userid
+where B.userid='jyp';
+
+--전체 회원들이 구매한 목록을 모두 출력하되, 회원 아이디 순으로 정렬
+select U.userid,U.username,B.prodname,U.addr,U.mobile1,U.mobile2 as "연락처"
+from usertbl U
+    inner join buytbl B -- inner join : 양쪽의 조건 모두 충족시켜야 조인,  outer join : 물건을 사지 않은사람까지 모두 포함시켜서 조인
+    on U.userid=B.userid;
+--where B.userid='jyp';
+
+select distinct U.userid,U.username,U.addr
+from usertbl U
+    inner join buytbl B
+    on U.userid=B.userid
+    order by U.userid;
     
+select U.userid,U.username,U.addr
+from usertbl U
+where exists(select * from buytbl B where U.userid=B.userid);
+
+commit;
+---------------------
+create table stdtbl(
+stdname nchar(5) not null primary key,
+addr nchar(2) not null
+);
+
+create table clubtbl(
+clubname nchar(5) not null primary key,
+roomno nchar(4) not null
+);
+
+create sequence stdclubseq;
+
+create table stdclubtbl(
+idnum number(5) not null primary key,
+stdname nchar(5) not null,
+clubname nchar(5) not null,
+foreign key(stdname) references stdtbl(stdname),
+foreign key(clubname) references clubtbl(clubname)
+);
+
+insert into stdtbl values('김범수','경남');
+insert into stdtbl values('성시경','서울');
+insert into stdtbl values('조용필','경기');
+insert into stdtbl values('은지원','경북');
+insert into stdtbl values('바비킴','서울');
+
+insert into clubtbl values('수영','101호');
+insert into clubtbl values('바둑','102호');
+insert into clubtbl values('축구','103호');
+insert into clubtbl values('봉사','104호');
+
+insert into stdclubtbl values(stdclubseq.nextval,'김범수','바둑');
+insert into stdclubtbl values(stdclubseq.nextval,'김범수','축구');
+insert into stdclubtbl values(stdclubseq.nextval,'조용필','축구');
+insert into stdclubtbl values(stdclubseq.nextval,'은지원','축구');
+insert into stdclubtbl values(stdclubseq.nextval,'은지원','봉사');
+insert into stdclubtbl values(stdclubseq.nextval,'바비킴','봉사');
+
+select * from clubtbl;
+select * from stdclubtbl;
+select * from stdtbl;
+
+--학생 테이블, 동아리 테이블, 학생동아리 테이블을 이용해서 학생을 기준으로 학생이름,지역,
+--가입한 동아리,방호수 조회 출력
+
+
+
+
+
+
+
+
+
+
 
 
 
